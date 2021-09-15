0fa2532f2747172a737a78d5b9a5775b93eadc60
TsimafeiTESTgit <90564062+TsimafeiTESTgit@users.noreply.github.com>
2021-09-15 17:43:34 +0300

    delete comment

--- src/ConvertToBinary.java
+++ src/ConvertToBinary.java
@@ -1,9 +1,14 @@
 public class ConvertToBinary {
-    public static void main(String[] args) {        //test numbers
-        int[] testNumbers = new int[]{10, 1, 8, 65, 255};        //go through the test numbers array
-        for (int i = 0; i < testNumbers.length; i++) {            //each number convert to binary
-            String binaryString = toBinary(testNumbers[i]);            //compare converter results with correct result
-            if (!Integer.toBinaryString(testNumbers[i]).equals(binaryString)) {                //incorrect result is printed and program stops
+    public static void main(String[] args) {        
+         //test numbers
+        int[] testNumbers = new int[]{10, 1, 8, 65, 255};        
+        //go through the test numbers array
+        for (int i = 0; i < testNumbers.length; i++) {                  // till it reach '.length'
+            //each number convert to binary
+            String binaryString = toBinary(testNumbers[i]);            // <---- 'testNumber' to 'toBinary' through cycle 'for'              +    Why we use STRING and not INTEGER tho???
+            //compare converter results with correct result
+            if (!Integer.toBinaryString(testNumbers[i]).equals(binaryString)) {
+                //incorrect result is printed and program stops
                 System.out.println("Incorrect converting of " + testNumbers[i]);
                 System.out.println("Expected: " + Integer.toBinaryString(testNumbers[i]));
                 System.out.println("Actual: " + binaryString);
@@ -16,7 +21,7 @@ public class ConvertToBinary {
         System.out.println("Successfully executed");
     }
 
-    private static String toBinary(int n) {
+    private static String toBinary(int n) {                             // <---- 'testNumber' to 'toBinary' through cycle 'for'
         String S = "";
         if (n == 0) {
             return "0";
@@ -27,4 +32,4 @@ public class ConvertToBinary {
         }
         return S;
     }
-}
+} 
