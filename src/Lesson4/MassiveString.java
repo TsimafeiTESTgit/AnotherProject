505a01ed28ea04f522d529f26d140cfaee32db07
TsimafeiTESTgit <offeredscrul@gmail.com>
2021-09-20 20:53:07 +0300

    less4 commit 2

--- src/Lesson4/MassiveString.java
+++ src/Lesson4/MassiveString.java
@@ -1,7 +1,19 @@
 package Lesson4;
 
+import java.lang.reflect.Array;
+import java.util.Random;
+
 public class MassiveString {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        String[] arr = new String[10];
+
+        for (int i = 0; i < arr.length; i++) {
+            String strI = Integer.toString(new Random().nextInt(10));
+            System.out.println("string "+ strI);
+            arr[i] = strI;
+        }
 
     }
+
 }
+
